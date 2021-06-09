a9741fd095d73bcba5d0e7b0c5632188a9a639ed
dbornilla <78960805+dbornilla@users.noreply.github.com>
2021-06-09 15:38:28 +0800

    add test 5

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,8 @@ public class Main {
         System.out.println("Hello I'm Carl");
         System.out.println("Hello I'm Dave");
         System.out.println("Ako si carl");
-        System.out.println("Test2");
+
         System.out.println("Test 4");
+        System.out.println("Test 5");
     }
 }
