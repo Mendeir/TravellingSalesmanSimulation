603d810b2c1285cec0d52360167f41d32c326cc0
MG <mg-git.plumbic@8shield.net>
2021-06-09 14:36:20 +0800

    Add "Ako si Carl"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args){
 
         System.out.println("Hello i'm MG");
         System.out.println("Hello I'm Carl");
+        System.out.println("Ako si carl");
     }
 }
