58c9e948b55021b30dc1a273e77839d953e72a29
dbornilla <78960805+dbornilla@users.noreply.github.com>
2021-06-12 18:26:08 +0800

    Store user's input values

--- src/InputWindow.java
+++ src/InputWindow.java
@@ -2,19 +2,28 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.SQLOutput;
 
 public class InputWindow extends JFrame implements ActionListener{
 
-    // ComboBox values
+    // array declarations
     String [] matrixSize = {"3x3","4x4","5x5","6x6"};
+    int [][] values;
+    int row;
+    int column;
+    int i = 0;
+    int j = 0;
 
     // Instantiation of objects for GUI
     JFrame frame = new JFrame("Traveling Salesman");
     JComboBox boxSize = new JComboBox(matrixSize);
     JPanel panel = new JPanel();
+    JButton right = new JButton(">>");
+    JButton left = new JButton("<<");
     JTextField[][] txt;
+    JButton [][] box;
     GridBagConstraints gbc = new GridBagConstraints();
+    GridBagLayout gbl = new GridBagLayout();
+
 
     //Constructor
     InputWindow()
@@ -28,8 +37,16 @@ public class InputWindow extends JFrame implements ActionListener{
 
         // Combobox design and simulations
         frame.add(boxSize);
-        boxSize.setBounds(100,20,200,25);
+        boxSize.setBounds(200,20,100,25);
         boxSize.addActionListener(this);
+
+        //buttons size and layout
+        frame.add(right);
+        frame.add(left);
+        right.addActionListener(this);
+        left.addActionListener(this);
+        right.setBounds(350,400,80,50);
+        left.setBounds(50,400,80,50);
 
         // Add panel and set frame visible
         frame.add(panel);
@@ -40,11 +57,9 @@ public class InputWindow extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e)
     {
-
+        // box size action
         if(e.getSource() == boxSize)
         {
-            int row;
-            int column;
 
             // Get the user's wanted size form the comboBox
             String size  = (String) boxSize.getSelectedItem();
@@ -54,23 +69,56 @@ public class InputWindow extends JFrame implements ActionListener{
             column = Integer.parseInt(s[1]);
             gridLayout(row,column);
         }
+
+        //right button action
+        if(e.getSource() == right)
+        {
+
+            panel.removeAll();
+            panel.revalidate();
+            frame.repaint();
+            values = new int[row][column];
+            // Get all value on each textFields
+            for(i = 0;i < row;i++){
+                for(j = 0;j < column;j++){
+                    if(i != j)
+                    {
+                        values[i][j] = Integer.parseInt(txt[i][j].getText());
+                    }else
+                    {
+                        values[i][j] = -1;
+                    }
+                }
+            }
+            //nextButton(row,column);
+        }
+
+        //left button action
+        if(e.getSource() == left)
+        {
+            panel.removeAll();
+            panel.revalidate();
+            frame.repaint();
+        }
     }
+
+    // layout for combo box options
     public void gridLayout(int row, int column){
         txt = new JTextField[row][column];
         panel.removeAll();
         panel.revalidate();
         frame.repaint();
 
-        for(int i=0; i<row; i++) {
-            for (int j = 0; j < column; j++) {
+        for(i=0; i<row; i++) {
+            for (j = 0; j < column; j++) {
                 if (i != j) {
-                    txt[i][j] = new JTextField(3);
+                    txt[i][j] = new JTextField();
                     gbc.gridx = i;
                     gbc.gridy = j;
                     panel.add(txt[i][j], gbc);
                     txt[i][j].setHorizontalAlignment(JTextField.CENTER);
                 } else {
-                    txt[i][j] = new JTextField("-", 3);
+                    txt[i][j] = new JTextField("-",3);
                     gbc.gridx = i;
                     gbc.gridy = j;
                     panel.add(txt[i][j], gbc);
@@ -79,5 +127,20 @@ public class InputWindow extends JFrame implements ActionListener{
                 }
             }
         }
+    }
+
+    public void nextButton(int row,int column)
+    {
+
+        /*
+        for(i=0;i<row;i++){
+            for(j=0;j<column;j++){
+                if(i != j)
+                {
+
+                }
+            }
+        }
+        */
     }
 }
